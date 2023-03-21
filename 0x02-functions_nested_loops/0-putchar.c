8fee063454be5f876969a0c40b7316755cb53492
Kutambang <paulinesanabax@gmail.com>
2023-03-21 12:52:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
-* main -  prints _putchar and a new line
-* Return: Always 0
-*/
+ * main - main entry _putchar
+ * Description - Prints _putchar followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-return (write(1, &c, 1));
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
